7b861cb1932924ffd6ea6529f0ce6894c354ea7c
HawraaYounes <hawraayounes0@gmail.com>
2022-09-16 17:00:38 +0300

    Organizing the code and adding score system

--- script.js
+++ script.js
@@ -1,77 +1,75 @@
-let duration = 1000;
-
+const duration = 1000;
 let blocksContainer = document.querySelector(".memory-game-blocks");
-
 let blocks = Array.from(blocksContainer.children);
 
 let orderRange = Array.from(Array(blocks.length).keys());
 shuffle(orderRange);
 blocks.forEach((block, index) => {
   block.style.order = orderRange[index];
+  block.addEventListener('click', function () {
+    flipBlock(block);
 
-
+  });
 });
 
+// Flip Block Function
 function flipBlock(selectedBlock) {
-    selectedBlock.classList.add('is-flipped');
-    let allFlippedBlocks = blocks.filter(flippedBlock => flippedBlock.classList.contains('is-flipped'));
-    if (allFlippedBlocks.length === 2) {
-      stopClicking();
-      checkMatchedBlocks(allFlippedBlocks[0], allFlippedBlocks[1]);
-  
-    }
-  
+  selectedBlock.classList.add('is-flipped');
+  let allFlippedBlocks = blocks.filter(flippedBlock => flippedBlock.classList.contains('is-flipped'));
+  if (allFlippedBlocks.length === 2) {
+    stopClicking();
+    checkMatchedBlocks(allFlippedBlocks[0], allFlippedBlocks[1]);
+
   }
-  function stopClicking() {
-    blocksContainer.classList.add('no-clicking');
+}
+
+// Stop Clicking Function
+function stopClicking() {
+  blocksContainer.classList.add('no-clicking');
+  setTimeout(() => {
+    blocksContainer.classList.remove('no-clicking');
+
+  }, duration);
+
+}
+
+// Check Matched Block
+function checkMatchedBlocks(firstBlock, secondBlock) {
+
+  let triesElement = document.querySelector('.tries span');
+  if (firstBlock.dataset.technology === secondBlock.dataset.technology) {
+        setTimeout(() => {
+            firstBlock.classList.remove('is-flipped');
+            secondBlock.classList.remove('is-flipped');
+
+            firstBlock.classList.add('has-match');
+            secondBlock.classList.add('has-match');
+        }, duration);
+
+  } else {
+
+    triesElement.innerHTML = parseInt(triesElement.innerHTML) + 1;
     setTimeout(() => {
-      blocksContainer.classList.remove('no-clicking');
-  
-    }, duration);
-  
-  }
 
-  function checkMatchedBlocks(firstBlock, secondBlock) {
-
-    let triesElement = document.querySelector('.tries span');
-  
-    if (firstBlock.dataset.technology === secondBlock.dataset.technology) {
-  
       firstBlock.classList.remove('is-flipped');
       secondBlock.classList.remove('is-flipped');
-  
-      firstBlock.classList.add('has-match');
-      secondBlock.classList.add('has-match');
-  
-      document.getElementById('success').play();
-  
-    } else {
-  
-      triesElement.innerHTML = parseInt(triesElement.innerHTML) + 1;
-  
-      setTimeout(() => {
-  
-        firstBlock.classList.remove('is-flipped');
-        secondBlock.classList.remove('is-flipped');
-  
-      }, duration);
-  
-      document.getElementById('fail').play();
-  
-    }
-  
-  }
-  
-  
+
+    }, duration);}
+
+}
+
+// Shuffle Function
 function shuffle(array) {
-    let current = array.length,temp,random;
-    while (current > 0) {
-      random = Math.floor(Math.random() * current);
-      current--;
-      temp = array[current];
-      array[current] = array[random];
-      array[random] = temp;
-  
-    }
-    return array;
+
+  let current = array.length,temp,random;
+
+  while (current > 0) {
+    random = Math.floor(Math.random() * current);
+    current--;
+    temp = array[current];
+    array[current] = array[random];
+    array[random] = temp;
+
   }
+  return array;
+}
