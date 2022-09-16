4a03690535c8b68c3f199ed7895b7ffc6bde9395
HawraaYounes <hawraayounes0@gmail.com>
2022-09-16 15:41:55 +0300

    Shuffling the cards

--- script.js
+++ script.js
@@ -1,60 +1,13 @@
-const cards = document.querySelectorAll('.memory-card');
+let duration = 1000;
 
-let hasFlippedCard = false;
-let lockBoard = false;
-let firstCard, secondCard;
+let blocksContainer = document.querySelector(".memory-game-blocks");
 
-function flipCard() {
-  if (lockBoard) return;
-  if (this === firstCard) return;
+let blocks = Array.from(blocksContainer.children);
 
-  this.classList.add('flip');
+let orderRange = Array.from(Array(blocks.length).keys());
+shuffle(orderRange);
+blocks.forEach((block, index) => {
+  block.style.order = orderRange[index];
 
-  if (!hasFlippedCard) {
-    hasFlippedCard = true;
-    firstCard = this;
 
-    return;
-  }
-
-  secondCard = this;
-  checkForMatch();
-}
-
-function checkForMatch() {
-  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-
-  isMatch ? disableCards() : unflipCards();
-}
-
-function disableCards() {
-  firstCard.removeEventListener('click', flipCard);
-  secondCard.removeEventListener('click', flipCard);
-
-  resetBoard();
-}
-
-function unflipCards() {
-  lockBoard = true;
-
-  setTimeout(() => {
-    firstCard.classList.remove('flip');
-    secondCard.classList.remove('flip');
-
-    resetBoard();
-  }, 1500);
-}
-
-function resetBoard() {
-  [hasFlippedCard, lockBoard] = [false, false];
-  [firstCard, secondCard] = [null, null];
-}
-
-(function shuffle() {
-  cards.forEach(card => {
-    let randomPos = Math.floor(Math.random() * 12);
-    card.style.order = randomPos;
-  });
-})();
-
-cards.forEach(card => card.addEventListener('click', flipCard));
+});
